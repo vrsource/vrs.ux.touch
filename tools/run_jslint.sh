4b8e3673ef29c751484fba822b0ab75e39a3ba73
Allen Bierbaum <abierbaum@gmail.com>
2011-04-19 13:52:27 -0500

    Enable flag that defines all global browser symbols by default.

--- tools/run_jslint.sh
+++ tools/run_jslint.sh
@@ -5,7 +5,7 @@
 JS_FILES=$(find src spec -name "*.js" -printf '%h/%f ' -type f)
 PACKAGE_PREDEFS='assert,Ext,vrs'
 TEST_PREDEFS='jasmine,AssertException,test,describe,it,expect,spyOn,beforeEach,afterEach,waitsFor,runs,_addStepToCurrentSpec,feature,story,component,concern,context,scenario,spec,given,when,then,and,but'
-DEFAULT_ARGS='--maxerr 9999 --eqeqeq --evil --forin --immed --maxlen 100 --undef --devel --onevar --bitwise --sub --newcap'
+DEFAULT_ARGS='--browser --maxerr 9999 --eqeqeq --evil --forin --immed --maxlen 100 --undef --devel --onevar --bitwise --sub --newcap'
 
 java -jar deps/jslint4java/jslint4java-1.4.4.jar $DEFAULT_ARGS --predef $PACKAGE_PREDEFS,$TEST_PREDEFS $@ $JS_FILES
 
