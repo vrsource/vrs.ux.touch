c2974d83633fab870cdbe851fc67d325f6042810
Allen Bierbaum <abierbaum@gmail.com>
2011-04-20 14:47:33 -0500

    Add a new simpler test.

---
+++ examples/simple_menupanel.js
@@ -0,0 +1,88 @@
+Ext.setup({
+onReady: function() {
+
+   menu_items = [
+      {content: 'Photo Gallery', leftIcon: 'img/flower.png', rightIcon: 'img/chevron.png'},
+      {content: 'Messaging', leftIcon: 'img/sms.png', rightIcon: 'img/chevron.png'},
+      {content: 'Allow Tracking', leftIcon: 'img/checked.png' }
+   ]
+
+   var menu = new vrs.ux.touch.MenuPanel({
+      menuItems: menu_items,
+      listeners: {
+         itemTap:       function(fl, index) { console.log('Tapped index: ', index); },
+         leftTap:       function(fl, index) { console.log('Left item selected: ', index); },
+         rightTap:      function(fl, index) { console.log('Right item selected: ', index); },
+      }
+   });
+
+   // Selection menu
+   var selection_menu = new vrs.ux.touch.MenuPanel({
+      listeners: {
+         itemTap: function(formList, index, el, ev) {
+            // Change the check to be on the one that was just selected
+            Ext.each(formList.menuItems, function(item, i) {
+               var left_icon_cls = ((i === index) ? 'check1' : null);
+               formList.updateItem(i, {leftIconCls: left_icon_cls});
+            });
+         }
+      },
+      menuItems: [
+         {leftIconCls: 'check1', content: 'Item 1'},
+         {content: 'Item 2'},
+         {content: 'Item 3'},
+         {content: 'Item 4'},
+         {content: 'Add Item...', itemtap: function() {alert('Add item');} },
+      ]
+   });
+
+   var panel = new Ext.form.FormPanel({
+      fullscreen: true,
+      scroll: 'vertical',
+      dockedItems: [
+         {
+            dock : 'top',
+            xtype: 'toolbar',
+            title: 'Menu Panel',
+         }
+      ],
+      items: [
+         {
+            xtype: 'fieldset',
+            title: 'Mixed Menu and Form Fields',
+            instructions: 'This shows how to mix menu in with other fields.',
+            items: [
+               menu,
+               {
+                  xtype: 'textfield',
+                  name: 'name',
+                  label: 'Name',
+                  placeHolder: 'Tom Roy',
+                  autoCapitalize : true,
+                  required: true,
+                  useClearIcon: true
+               }, {
+                  xtype: 'togglefield',
+                  name: 'enable',
+                  label: 'Enable'
+               }, {
+                   xtype: 'passwordfield',
+                   name: 'password',
+                   label: 'Password',
+                   placeHolder: 'Select each time',
+                   useClearIcon: true
+               }
+            ]
+         },
+         {
+            xtype: 'fieldset',
+            title: 'Selection Menu',
+            instructions: 'This panel allows selection of the menu items.',
+            items: [selection_menu]
+         }
+      ]
+
+   });
+}
+});
+
