2c0e54872aba759d3ee83638cfe3c8ba7cb1c299
Nathan Landis <my8bird@gmail.com>
2012-01-26 20:13:24 -0600

    Get the testing infrastructure ready

---
+++ spec/leaflet_spec.js
@@ -0,0 +1,19 @@
+
+describe('LeafletMap Ext Component', function() {
+   describe('Basics', function() {
+      var Map = vrs.ux.touch.LeafletMap;
+
+      it('provides new mapping events', function() {
+         var map = new Map();
+
+         expect(map.events.repPicked).toBeDefined();
+      });
+
+      it('provides new mapping events', function() {
+         var map = new Map();
+
+         expect(map.events.repPicked).toBeDefined();
+      });
+
+   });
+});
