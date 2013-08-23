e8a2ff81f94cc9f8051bc2a9746e49963c75c8b8
Allen Bierbaum <abierbaum@gmail.com>
2013-08-23 10:59:32 -0500

    Add a DialogController class to help with dialog usage.

--- src/vrs.AppCtrl.js
+++ src/vrs.AppCtrl.js
@@ -301,6 +301,80 @@ Ext.define('vrs.PanelController', {
 
 
 /**
+* Extension to PanelController that allows for dialog display
+* and interaction.
+*/
+Ext.define('vrs.DialogController', {
+   extend: 'vrs.PanelController',
+
+   config: {
+
+      /**
+      * Callback function which is called when dialog is dismissed by interaction.
+      *
+      *  params:
+      *    - buttonId: (string) id of the button as defined by the specific dialog
+      *    - dialog: (obj) reference to this object
+      */
+      completeFn: null,
+
+      /**
+      * The scope of the callback function
+      */
+      scope: null,
+
+      /**
+      * If true, then we are modal during interaction.
+      */
+      modal: true,
+
+      /** If true, center the dialog. */
+      centered: true,
+
+      width  : null,
+      height : null
+   },
+
+   show: function(options) {
+      var panel = this.getPanel();
+
+      if(!panel.getParent() && Ext.Viewport) {
+         Ext.Viewport.add(panel);
+      }
+
+      this.setConfig(options);
+
+      panel.setConfig({
+         modal    : this.getModal(),
+         centered : this.getCentered(),
+         width    : this.getWidth(),
+         height   : this.getHeight()
+      });
+      panel.show();
+   },
+
+   _finishDialog: function(buttonId) {
+      var fn = this.getCompleteFn(),
+          panel = this.getPanel(),
+          me = this;
+
+      // If we have a completeFn, then setup to call it once the dialog
+      // component is hidden
+      if(_.isFunction(fn)) {
+         panel.on({
+            hiddenchange: function() {
+               fn.call(me.getScope() || me, buttonId, me);
+            },
+            single: true
+         });
+      }
+
+      panel.hide();
+   }
+});
+
+
+/**
 * Slightly customized PanelController that knows how to act as
 * a child of a stack holder (PanelHolder).
 *
